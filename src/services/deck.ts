acc886056dbbcc56c7484bb8379634fe360a8ac4
Jens Kolind <jkolind@ibsoft-us.com>
2016-12-27 22:38:02 -0600

    Add documentation to the services

--- src/services/deck.ts
+++ src/services/deck.ts
@@ -1,161 +1,212 @@
 export const Ranks = {
-    '2': 'Two',
-    '3': 'Three',
-    '4': 'Four',
-    '5': 'Five',
-    '6': 'Six',
-    '7': 'Seven',
-    '8': 'Eight',
-    '9': 'Nine',
-    '10': 'Ten',
-    'J': 'Jack',
-    'Q': 'Queen',
-    'K': 'King',
-    'A': 'Ace',
-    'N': 'Joker'
+	'2': 'Two',
+	'3': 'Three',
+	'4': 'Four',
+	'5': 'Five',
+	'6': 'Six',
+	'7': 'Seven',
+	'8': 'Eight',
+	'9': 'Nine',
+	'10': 'Ten',
+	'J': 'Jack',
+	'Q': 'Queen',
+	'K': 'King',
+	'A': 'Ace',
+	'N': 'Joker'
 }
 
 export const Suits = {
-    'S': 'Spades',
-    'D': 'Diamonds',
-    'C': 'Clubs',
-    'H': 'Hearts'  
+	'S': 'Spades',
+	'D': 'Diamonds',
+	'C': 'Clubs',
+	'H': 'Hearts'  
 }
 
 export const defaultDeckConfiguration = {
-    decks: 1,
-    jokers: 2,
-    ranks: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ],
-    suits: [ 'S', 'D', 'C', 'H' ]
+	decks: 1,
+	jokers: 2,
+	ranks: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ],
+	suits: [ 'S', 'D', 'C', 'H' ]
 }
 
 export const beziqueDeckConfiguration = {
-    decks: 2,
-    jokers: 0,
-    ranks: [ '7', '8', '9', 'J', 'Q', 'K', '10', 'A' ],
-    suits: [ 'S', 'D', 'C', 'H' ]
+	decks: 2,
+	jokers: 0,
+	ranks: [ '7', '8', '9', 'J', 'Q', 'K', '10', 'A' ],
+	suits: [ 'S', 'D', 'C', 'H' ]
 }
 
+/**
+ * Playing card class
+ */
 export class PlayingCard {
-    public owningStack : CardStack;
+  /** Stack that owns this card */
+	public owningStack : CardStack;
 
-    constructor (public rank : string, public suit : string) {
-    }
+  /** Selected card */
+	public selected : boolean = false;
 
-    public selected : boolean = false;
+	constructor (public rank : string, public suit : string) {
+	}
 
-    public shortName() {
-        return this.rank + this.suit;
-    }
+	public moveToStack(newStack) {
+		if (this.owningStack) {
+			this.owningStack.remove(this);
+		}
+		newStack.add(this);
+	}
 
-    public toString() {
-        return this.shortName();
-    }
+  /** Short string (eg 8C) representation of the card */
+	public shortName() {
+		return this.rank + this.suit;
+	}
 
-    public longName() {
-        return Ranks[this.rank] + ' of ' + Suits[this.suit];
-    }
+  /** Long string (eg Eight of Clubs) representation of the card */
+	public longName() {
+		return Ranks[this.rank] + ' of ' + Suits[this.suit];
+	}
 
-    public moveToStack(newStack) {
-        if (this.owningStack) {
-            this.owningStack.remove(this);
-        }
-        newStack.add(this);
-    }
+  /** Default (short name) string representation */
+	public toString() {
+		return this.shortName();
+	}
 }
 
+/**
+ * Card stack class - used for decks, hands, melds, winning discarded cards, etc
+ */
 export class CardStack {
-    public stack : PlayingCard[] = [];
 
-    public add(playingCard : PlayingCard, changeOwner : Boolean = true) {
-        if (playingCard) {
-            this.stack.push(playingCard);
-            if (changeOwner) {
-                playingCard.owningStack = this;
-            }
-        }
-    }
+  /** The cards in the stack */
+	public stack : PlayingCard[] = [];
 
-    public remove(playingCard : PlayingCard, changeOwner : Boolean = true) {
-        var selectedCardIndex = this.stack.indexOf(playingCard);
-        if (selectedCardIndex > -1) {
-            this.stack.splice(selectedCardIndex, 1);
-            if (changeOwner) {
-                playingCard.owningStack = undefined;
-            }
-            return playingCard;
-        } else {
-            return undefined;
-        }
-    }
+  /** Add a card to the stack
+   * @param playingCard - the card to ad to the stack
+   * @param changeOwner - change the owner of the stack (default true)
+   */
+	public add(playingCard : PlayingCard, changeOwner : Boolean = true) {
+		if (playingCard) {
+			this.stack.push(playingCard);
+			if (changeOwner) {
+				playingCard.owningStack = this;
+			}
+		}
+	}
 
-    public draw() {
-        var drawnCard = this.stack.shift();
-        if (drawnCard) {
-            drawnCard.owningStack = undefined;
-        }
-        return drawnCard;
-    }
+  /**
+   * Remove th card from the stack
+   * @param playingCard - The card to remove
+   * @param changeOwner - If true change the owner of the card
+   * @return PlayingCard - the card that was removed. If the card is not in the stack this returns undefined
+   */
+	public remove(playingCard : PlayingCard, changeOwner : Boolean = true) {
+		var selectedCardIndex = this.stack.indexOf(playingCard);
+		if (selectedCardIndex > -1) {
+			this.stack.splice(selectedCardIndex, 1);
+			if (changeOwner) {
+				playingCard.owningStack = undefined;
+			}
+			return playingCard;
+		} else {
+			return undefined;
+		}
+	}
 
-    public swapCards(oldPosition, newPosition) {
-        var oldCard = this.stack[oldPosition];
-        this.stack[oldPosition] = this.stack[newPosition];
-        this.stack[newPosition] = oldCard;
-    }
+  /**
+   * Draw a card from the stack
+   * @return the card drawn (if one was available)
+   */
+	public draw() {
+		var drawnCard = this.stack.shift();
+		if (drawnCard) {
+			drawnCard.owningStack = undefined;
+		}
+		return drawnCard;
+	}
 
-    public clear() {
-        this.stack = [];
-    }
+  /**
+   * Swap cards in the stack (used for shuffling)
+   */
+	public swapCards(oldPosition, newPosition) {
+		var oldCard = this.stack[oldPosition];
+		this.stack[oldPosition] = this.stack[newPosition];
+		this.stack[newPosition] = oldCard;
+	}
 
-    public isEmpty() {
-        return (this.stack.length == 0);
-    }
+  /**
+   * Empty the stack
+   */
+	public clear() {
+		this.stack = [];
+	}
 
-    public toString() {
-        return this.stack.join(', ');
-    }
+  /**
+   * Returns true if the stack is empty
+   */
+	public isEmpty() {
+		return (this.stack.length == 0);
+	}
+
+  /**
+   * Returns a comma delimited string representation of the stack (eg "AC, KD")
+   */
+	public toString() {
+		return this.stack.join(', ');
+	}
 
 }
 
+/**
+ * A deck of cards
+ */
 export class Deck {
-    public playingCards: CardStack = new CardStack();
-    constructor (private config) {
-        this.resetDeck();
-        this.shuffle();
-    }
 
-    public resetDeck() {
-        this.playingCards = new CardStack();
-        // For each deck
-        for (var deck = 0; deck < this.config.decks; deck++) {
-            // For each rank
-            for (var rank in this.config.ranks) {
-                // For each suit
-                for (var suit in this.config.suits) {
-                    // Create a playing card and put it in the deck
-                    this.playingCards.add(new PlayingCard(this.config.ranks[rank], this.config.suits[suit]));
-                }
-            }
+  /** The playing cards in the deck */
+	public playingCards: CardStack = new CardStack();
 
-            // For each joker
-            for (var joker = 0; joker < this.config.jokers; joker++) {
-                this.playingCards.add(new PlayingCard('N', undefined));
-            }
-        }
-    }
+  /** Create the deck
+   * @param config - the configuration to use for the deck
+   */
+	constructor (private config) {
+		this.resetDeck();
+		this.shuffle();
+	}
 
-    public shuffle(numberOfTimes = 5) {
-        var randomPosition = 0;
-        for (var shuffleRound = 0; shuffleRound < numberOfTimes; shuffleRound++) {
-            for (var cardNumber = 0; cardNumber < this.playingCards.stack.length; cardNumber++) {
-                randomPosition = Math.floor(Math.random() * this.playingCards.stack.length);
-                this.playingCards.swapCards(cardNumber, randomPosition)
-                
-            }
-        }
-    }
+  /** Create the deck according to the configuration */
+	public resetDeck() {
+		this.playingCards = new CardStack();
+		// For each deck
+		for (var deck = 0; deck < this.config.decks; deck++) {
+			// For each rank
+			for (var rank in this.config.ranks) {
+				// For each suit
+				for (var suit in this.config.suits) {
+					// Create a playing card and put it in the deck
+					this.playingCards.add(new PlayingCard(this.config.ranks[rank], this.config.suits[suit]));
+				}
+			}
 
-    public draw() : PlayingCard {
-        return this.playingCards.draw();
-    }
+			// For each joker
+			for (var joker = 0; joker < this.config.jokers; joker++) {
+				this.playingCards.add(new PlayingCard('N', undefined));
+			}
+		}
+	}
+
+  /** Shuffle the deck */
+	public shuffle(numberOfTimes = 5) {
+		var randomPosition = 0;
+		for (var shuffleRound = 0; shuffleRound < numberOfTimes; shuffleRound++) {
+			for (var cardNumber = 0; cardNumber < this.playingCards.stack.length; cardNumber++) {
+				randomPosition = Math.floor(Math.random() * this.playingCards.stack.length);
+				this.playingCards.swapCards(cardNumber, randomPosition)
+				
+			}
+		}
+	}
+
+  /** Draw a card from the deck */
+	public draw() : PlayingCard {
+		return this.playingCards.draw();
+	}
 }
--- src/services/player.ts
+++ src/services/player.ts
@@ -3,79 +3,101 @@ import { CardStack, PlayingCard } from './deck';
 import { CardGame } from './game';
 
 export const defaultPlayerConfiguration = {
-    minHandLength : 0,
-    maxHandLength : 8,
-    minPlayableCards: 1,
-    maxPlayableCards: 1,
-    minMeldableCards: 2,
-    maxMeldableCards: 8
+	minHandLength : 0,
+	maxHandLength : 8,
+	minPlayableCards: 1,
+	maxPlayableCards: 1,
+	minMeldableCards: 2,
+	maxMeldableCards: 8
 }
 
+/**
+ * A card player
+ */
 export class CardPlayer {
-    public hand : CardStack = new CardStack();
-    public selectedCards: CardStack = new CardStack();
-    public playedCards : CardStack = new CardStack();
-    public wonCards: CardStack = new CardStack();
-    public melds: CardStack = new CardStack();
-    public availableMelds: CardStack = new CardStack();
+  /** The cards in the hand (not visible) */
+	public hand : CardStack = new CardStack();
 
-    constructor (public name : string, public game: CardGame, private playerConfig) {
-    }
+  /** The cards played in this round (visible) */
+	public playedCards : CardStack = new CardStack();
 
-    public canDraw() {
-        return this.hand.stack.length + this.melds.stack.length < this.playerConfig.maxHandLength &&
-            this.game.dealt && 
-            this.game.deck.playingCards.stack.length > 0;
-    }
+  /** The melds that have been played (visible) */
+	public melds: CardStack = new CardStack();
 
-    public draw(numberOfCards : Number = 1) {
-        for (var cardNumber = 0; cardNumber < numberOfCards; cardNumber++) {
-            this.hand.add(this.game.deck.draw());
-        }
-    }
+  /** The cards previously won */
+	public wonCards: CardStack = new CardStack();
 
-    public toggleSelect(playingCard : PlayingCard) {
-        if (playingCard.selected) {
-            this.selectedCards.remove(playingCard, false);
-        } else {
-            this.selectedCards.add(playingCard, false);
-        }
-    }
+  /** The cards that can be used in melds */
+	public availableMelds: CardStack = new CardStack();
 
-    public canPlay() {
-        return this.selectedCards.stack.length >= this.playerConfig.minPlayableCards &&
-            this.selectedCards.stack.length <= this.playerConfig.maxPlayableCards &&
-            this.game.inTurn === this && !this.canDraw();
-    }
+  /** The cards that have been selected for a meld or to play (not visible) */
+	public selectedCards: CardStack = new CardStack();
 
-    public play() {
-        this.selectedCards.stack.forEach((card) => {
-            card.selected = false;
-            card.moveToStack(this.playedCards);
-        });
-        this.selectedCards.clear();
-        this.game.nextTurn()
-    }
+	constructor (public name : string, public game: CardGame, private playerConfig) {
+	}
 
-    public canMeld() {
-        this.calculateMelds();
-        return this.availableMelds.isEmpty();
-        /*
-        return this.game.inTurn === this && 
-            this.selectedCards.stack.length >= this.playerConfig.minMeldableCards &&
-            this.selectedCards.stack.length <= this.playerConfig.maxMeldableCards;
-        */
-    }
+  /** Returns true if it is legal to draw on the next turn */
+	public canDraw() {
+		return this.hand.stack.length + this.melds.stack.length < this.playerConfig.maxHandLength &&
+			this.game.dealt && 
+			this.game.deck.playingCards.stack.length > 0;
+	}
 
-    public meld() {
-        this.selectedCards.stack.forEach((card) => {
-            card.selected = false;
-            card.moveToStack(this.melds);
-        });
-        this.selectedCards.clear();        
-    }
-    
-    public calculateMelds() {
-        
-    }
+  /** Draw cards from the game deck */
+	public draw(numberOfCards : Number = 1) {
+		for (var cardNumber = 0; cardNumber < numberOfCards; cardNumber++) {
+			this.hand.add(this.game.deck.draw());
+		}
+	}
+
+  /** Returns true if it is legal to play selected cards on the next turn */
+	public canPlay() {
+		return this.selectedCards.stack.length >= this.playerConfig.minPlayableCards &&
+			this.selectedCards.stack.length <= this.playerConfig.maxPlayableCards &&
+			this.game.inTurn === this && !this.canDraw();
+	}
+
+  /** Play the cards */
+	public play() {
+		this.selectedCards.stack.forEach((card) => {
+			card.selected = false;
+			card.moveToStack(this.playedCards);
+		});
+		this.selectedCards.clear();
+		this.game.nextTurn()
+	}
+
+  /** Returns true if it is legal to meld selected cards on the next turn */
+	public canMeld() {
+		this.calculateMelds();
+		return this.availableMelds.isEmpty();
+		/*
+		return this.game.inTurn === this && 
+			this.selectedCards.stack.length >= this.playerConfig.minMeldableCards &&
+			this.selectedCards.stack.length <= this.playerConfig.maxMeldableCards;
+		*/
+	}
+
+  /** Meld the selected cards */
+	public meld() {
+		this.selectedCards.stack.forEach((card) => {
+			card.selected = false;
+			card.moveToStack(this.melds);
+		});
+		this.selectedCards.clear();        
+	}
+	
+	public calculateMelds() {
+		
+	}
+
+  /** Select / unselect a playing card */
+	public toggleSelect(playingCard : PlayingCard) {
+		if (playingCard.selected) {
+			this.selectedCards.remove(playingCard, false);
+		} else {
+			this.selectedCards.add(playingCard, false);
+		}
+	}
+
 }
