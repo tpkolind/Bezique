9e5f0759e4386427fe11d9bbb7062cc8fd21247f
Jens Kolind <jkolind@ibsoft-us.com>
2017-01-01 15:56:00 -0600

    Separate out playing card

--- src/services/deck.ts
+++ src/services/deck.ts
@@ -1,26 +1,4 @@
-export const Ranks = {
-	'2': 'Two',
-	'3': 'Three',
-	'4': 'Four',
-	'5': 'Five',
-	'6': 'Six',
-	'7': 'Seven',
-	'8': 'Eight',
-	'9': 'Nine',
-	'10': 'Ten',
-	'J': 'Jack',
-	'Q': 'Queen',
-	'K': 'King',
-	'A': 'Ace',
-	'N': 'Joker'
-}
-
-export const Suits = {
-	'S': 'Spades',
-	'D': 'Diamonds',
-	'C': 'Clubs',
-	'H': 'Hearts'  
-}
+import { PlayingCard } from './playing-card'
 
 export const defaultDeckConfiguration = {
 	decks: 1,
@@ -36,60 +14,6 @@ export const beziqueDeckConfiguration = {
 	ranks: [ '7', '8', '9', 'J', 'Q', 'K', '10', 'A' ],
 	suits: [ 'S', 'D', 'C', 'H' ],
 	trump: true
-}
-
-/**
- * Playing card class
- */
-export class PlayingCard {
-  /** Stack that owns this card */
-	public owningStack : CardStack;
-
-  /** Selected card */
-	public selected : boolean = false;
-
-	constructor (public rank : string, public suit : string) {
-	}
-
-	public toggleSelect(selectionStack : CardStack, multiSelect : boolean = false) {
-		var previouslySelected = this.selected;
-		if (selectionStack) {
-			if (!multiSelect) {
-				selectionStack.stack.forEach((card) => {
-					card.selected = false;
-					selectionStack.remove(card);
-				});
-			}
-			this.selected = !previouslySelected;
-			if (this.selected) {
-				selectionStack.add(this);
-			} else {
-				selectionStack.remove(this);
-			}
-		}
-	}
-
-	public moveToStack(newStack) {
-		if (this.owningStack) {
-			this.owningStack.remove(this);
-		}
-		newStack.add(this);
-	}
-
-  /** Short string (eg 8C) representation of the card */
-	public shortName() {
-		return this.rank + this.suit;
-	}
-
-  /** Long string (eg Eight of Clubs) representation of the card */
-	public longName() {
-		return Ranks[this.rank] + ' of ' + Suits[this.suit];
-	}
-
-  /** Default (short name) string representation */
-	public toString() {
-		return this.shortName();
-	}
 }
 
 /**
